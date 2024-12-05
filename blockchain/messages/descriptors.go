ecad49942d126dc1a0e59ec001e367cd1124e22d
studnev <astudnev@gmail.com>
2024-12-05 16:12:15 +0200

    descriptor

--- blockchain/messages/descriptors.go
+++ blockchain/messages/descriptors.go
@@ -19,3 +19,18 @@ type BlockMessageDescriptor struct {
 	ChainId           string
 	TransactionHashes []string `mapstructure:",omitempty" json:",omitempty"`
 }
+
+/*
+For Solana height is not equal to block number
+For EVM blockchains they are the same and we do not send Height in descriptor
+*/
+func (d BlockDescriptor) BlockHeight() int64 {
+	if d.Height != 0 {
+		return int64(d.Height)
+	}
+	return int64(d.BlockNumber)
+}
+
+func (descriptor *BlockMessageDescriptor) IsBroadcasted() bool {
+	return len(descriptor.TransactionHashes) > 0
+}
