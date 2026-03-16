d65d5ad464718fc83498c4c0e521db860a80168c
studnev <astudnev@gmail.com>
2026-03-16 17:37:46 +0200

    double CirculatingSupply = 10;   double TotalSupply = 11;   double MaxSupply = 12;

--- market/python/market/marketdata_pb2.py
+++ market/python/market/marketdata_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17market/marketdata.proto\x12\x13marketdata_messages\"a\n\x11MarketdataMessage\x12<\n\nCurrencies\x18\x01 \x03(\x0b\x32(.marketdata_messages.CurrencyPriceBucket\x12\x0e\n\x06Source\x18\x02 \x01(\t\"\xb9\x01\n\x05Token\x12\n\n\x02Id\x18\x01 \x01(\t\x12\x0b\n\x03\x44id\x18\x02 \x01(\t\x12\x0f\n\x07Network\x18\x03 \x01(\t\x12\x14\n\x07\x41\x64\x64ress\x18\x04 \x01(\tH\x00\x88\x01\x01\x12\x14\n\x07TokenId\x18\x05 \x01(\tH\x01\x88\x01\x01\x12\x10\n\x08IsNative\x18\x06 \x01(\x08\x12\x0c\n\x04Name\x18\x07 \x01(\t\x12\x0e\n\x06Symbol\x18\x08 \x01(\t\x12\x12\n\nNetworkBid\x18\t \x01(\tB\n\n\x08_AddressB\n\n\x08_TokenId\"\xa5\x01\n\x13\x43urrencyPriceBucket\x12*\n\x06Tokens\x18\x01 \x03(\x0b\x32\x1a.marketdata_messages.Token\x12\x0c\n\x04Name\x18\x02 \x01(\t\x12\x0e\n\x06Symbol\x18\x03 \x01(\t\x12\x12\n\nOriginalId\x18\x04 \x01(\t\x12\x30\n\x06\x42ucket\x18\x06 \x01(\x0b\x32 .marketdata_messages.PriceBucket\"\"\n\x05Price\x12\x0c\n\x04Time\x18\x01 \x01(\x04\x12\x0b\n\x03Usd\x18\x02 \x01(\x01\"b\n\x0bPriceBucket\x12\x10\n\x08Interval\x18\x01 \x01(\r\x12*\n\x06Prices\x18\x02 \x03(\x0b\x32\x1a.marketdata_messages.Price\x12\x15\n\rLastUpdatedAt\x18\x03 \x01(\x04\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17market/marketdata.proto\x12\x13marketdata_messages\"a\n\x11MarketdataMessage\x12<\n\nCurrencies\x18\x01 \x03(\x0b\x32(.marketdata_messages.CurrencyPriceBucket\x12\x0e\n\x06Source\x18\x02 \x01(\t\"\xfc\x01\n\x05Token\x12\n\n\x02Id\x18\x01 \x01(\t\x12\x0b\n\x03\x44id\x18\x02 \x01(\t\x12\x0f\n\x07Network\x18\x03 \x01(\t\x12\x14\n\x07\x41\x64\x64ress\x18\x04 \x01(\tH\x00\x88\x01\x01\x12\x14\n\x07TokenId\x18\x05 \x01(\tH\x01\x88\x01\x01\x12\x10\n\x08IsNative\x18\x06 \x01(\x08\x12\x0c\n\x04Name\x18\x07 \x01(\t\x12\x0e\n\x06Symbol\x18\x08 \x01(\t\x12\x12\n\nNetworkBid\x18\t \x01(\t\x12\x19\n\x11\x43irculatingSupply\x18\n \x01(\x01\x12\x13\n\x0bTotalSupply\x18\x0b \x01(\x01\x12\x11\n\tMaxSupply\x18\x0c \x01(\x01\x42\n\n\x08_AddressB\n\n\x08_TokenId\"\xa5\x01\n\x13\x43urrencyPriceBucket\x12*\n\x06Tokens\x18\x01 \x03(\x0b\x32\x1a.marketdata_messages.Token\x12\x0c\n\x04Name\x18\x02 \x01(\t\x12\x0e\n\x06Symbol\x18\x03 \x01(\t\x12\x12\n\nOriginalId\x18\x04 \x01(\t\x12\x30\n\x06\x42ucket\x18\x06 \x01(\x0b\x32 .marketdata_messages.PriceBucket\"5\n\x05Price\x12\x0c\n\x04Time\x18\x01 \x01(\x04\x12\x0b\n\x03Usd\x18\x02 \x01(\x01\x12\x11\n\tMarketCap\x18\x03 \x01(\x01\"b\n\x0bPriceBucket\x12\x10\n\x08Interval\x18\x01 \x01(\r\x12*\n\x06Prices\x18\x02 \x03(\x0b\x32\x1a.marketdata_messages.Price\x12\x15\n\rLastUpdatedAt\x18\x03 \x01(\x04\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -34,11 +34,11 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_MARKETDATAMESSAGE']._serialized_start=48
   _globals['_MARKETDATAMESSAGE']._serialized_end=145
   _globals['_TOKEN']._serialized_start=148
-  _globals['_TOKEN']._serialized_end=333
-  _globals['_CURRENCYPRICEBUCKET']._serialized_start=336
-  _globals['_CURRENCYPRICEBUCKET']._serialized_end=501
-  _globals['_PRICE']._serialized_start=503
-  _globals['_PRICE']._serialized_end=537
-  _globals['_PRICEBUCKET']._serialized_start=539
-  _globals['_PRICEBUCKET']._serialized_end=637
+  _globals['_TOKEN']._serialized_end=400
+  _globals['_CURRENCYPRICEBUCKET']._serialized_start=403
+  _globals['_CURRENCYPRICEBUCKET']._serialized_end=568
+  _globals['_PRICE']._serialized_start=570
+  _globals['_PRICE']._serialized_end=623
+  _globals['_PRICEBUCKET']._serialized_start=625
+  _globals['_PRICEBUCKET']._serialized_end=723
 # @@protoc_insertion_point(module_scope)
